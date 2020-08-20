a647e753d4bc2ec3a54c44428b8edfa1a39ece43
Genji <ahandsel@gmail.com>
2020-08-20 10:10:22 +0900

    Works!

--- Pictorial_Stacked_Chart/K_PicStackChart.js
+++ Pictorial_Stacked_Chart/K_PicStackChart.js
@@ -15,9 +15,6 @@
     // Initiate the Themes
     am4core.useTheme(am4themes_animated);
 
-    // import SVG data from K_SVG.js file
-    var iconPath = imgData;
-
     // Create chart instance
     var chart = am4core.create(spaceDiv, am4charts.SlicedChart);
     // initial fade in effect
@@ -38,14 +35,26 @@
     series.dataFields.category = "name";
     series.alignLabels = true;
 
-    series.maskSprite.path = iconPath;
+    // Set the SVG data (from K_SVG.js)
+    series.maskSprite.path = svgCan;
+
+    // Ticks connect slice to its label
     series.ticks.template.locationX = 1;
     series.ticks.template.locationY = 0.5;
 
+    // Container label elements are put in.
     series.labelsContainer.width = 200;
 
     chart.legend = new am4charts.Legend();
     chart.legend.position = "left";
     chart.legend.valign = "bottom";
+
+    // --- Optional ---
+
+    // Enable export
+    chart.exporting.menu = new am4core.ExportMenu();
+    chart.exporting.menu.align = "left";
+    chart.exporting.menu.verticalAlign = "top";
+    chart.exporting.backgroundColor = am4core.color("#f00", 0);
   }); // end am4core.ready()
 })();
