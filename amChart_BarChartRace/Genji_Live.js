a73cf628a49102ad5213da6818f43d3bd77dea12
Genji <ahandsel@gmail.com>
2020-09-28 17:09:34 +0900

    Single to double quotes & Reformating kintone.api

--- amChart_BarChartRace/Genji_Live.js
+++ amChart_BarChartRace/Genji_Live.js
@@ -21,16 +21,16 @@
 
     // Shorting numbers by units: K, M, B (Thousands, Millions, Billions)
     chart.numberFormatter.bigNumberPrefixes = [{
-        "number": 1e+3,
-        "suffix": "K"
+        'number': 1e+3,
+        'suffix': 'K'
       },
       {
-        "number": 1e+6,
-        "suffix": "M"
+        'number': 1e+6,
+        'suffix': 'M'
       },
       {
-        "number": 1e+9,
-        "suffix": "B"
+        'number': 1e+9,
+        'suffix': 'B'
       }
     ];
 
@@ -38,8 +38,8 @@
     var label = chart.plotContainer.createChild(am4core.Label);
     label.x = am4core.percent(97);
     label.y = am4core.percent(95);
-    label.horizontalCenter = "right";
-    label.verticalCenter = "middle";
+    label.horizontalCenter = 'right';
+    label.verticalCenter = 'middle';
     label.dx = -15;
     label.fontSize = 50;
 
@@ -48,7 +48,7 @@
     // Left side content with company names
     var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
     categoryAxis.renderer.grid.template.location = 0;
-    categoryAxis.dataFields.category = "Manufacturer";
+    categoryAxis.dataFields.category = 'Manufacturer';
     categoryAxis.renderer.minGridDistance = 1;
     categoryAxis.renderer.inversed = true;
     categoryAxis.renderer.grid.template.disabled = true;
@@ -62,26 +62,26 @@
 
     // Y-axis unit information
     var series = chart.series.push(new am4charts.ColumnSeries());
-    series.dataFields.categoryY = "Manufacturer";
-    series.dataFields.valueX = "value";
-    series.tooltipText = "{valueX.value}";
+    series.dataFields.categoryY = 'Manufacturer';
+    series.dataFields.valueX = 'value';
+    series.tooltipText = '{valueX.value}';
     series.columns.template.strokeOpacity = 0;
     series.columns.template.column.cornerRadiusBottomRight = 5;
     series.columns.template.column.cornerRadiusTopRight = 5;
     series.interpolationDuration = stepDuration;
     series.interpolationEasing = am4core.ease.linear;
 
-    // The "bar" changing shape
+    // The 'bar' changing shape
     var labelBullet = series.bullets.push(new am4charts.LabelBullet());
-    labelBullet.label.horizontalCenter = "right";
-    labelBullet.label.text = "{values.valueX.workingValue.formatNumber('#.0as')}";
-    labelBullet.label.textAlign = "end";
+    labelBullet.label.horizontalCenter = 'right';
+    labelBullet.label.text = '{values.valueX.workingValue.formatNumber(\'#.0as\')}';
+    labelBullet.label.textAlign = 'end';
     labelBullet.label.dx = -10;
 
     chart.zoomOutButton.disabled = true;
 
     // Color is set to columns with chart.colors color set
-    series.columns.template.adapter.add("fill", function (fill, target) {
+    series.columns.template.adapter.add('fill', function (fill, target) {
       return chart.colors.getIndex(target.dataItem.index);
     });
 
@@ -131,12 +131,14 @@
       chart.invalidateRawData();
       label.text = year.toString();
 
+      // Set the current zoom (data range) with the start & end values set between 0 to 1 (beginning to end)
       categoryAxis.zoom({
         start: 0,
         end: itemsWithNonZero / categoryAxis.dataItems.length
       });
     }
 
+    // Resort the series based on new data in descending order
     categoryAxis.sortBySeries = series;
 
     // Kintone REST API Request
@@ -153,44 +155,44 @@
     // Array [Array] based on year, Manufacturer: value
     // On slide - Algorithm to generate this output
     // Default Field Codes: Year = Number; Manufacturer = Drop_down; PhonesSold = Number_0
-    kintone.api(kintone.api.url('/k/v1/records', true), 'GET', body, function (resp) {
-      // Success
+    kintone.api(kintone.api.url('/k/v1/records', true), 'GET', body,
+      function (resp) {
+        // Successful API Call
 
-      var records = resp.records;
-      records.forEach(function (record) {
+        var records = resp.records;
+        records.forEach(function (record) {
 
-        if (allData.hasOwnProperty(record.Number.value)) {
-          allData[record.Number.value].push({
+          if (allData.hasOwnProperty(record.Number.value)) {
+            allData[record.Number.value].push({
+              Manufacturer: record.Drop_down.value,
+              value: record.Number_0.value
+            });
+            return;
+          }
+
+          allData[record.Number.value] = [{
             Manufacturer: record.Drop_down.value,
             value: record.Number_0.value
-          });
-          return;
-        }
+          }];
+        });
 
-        allData[record.Number.value] = [{
-          Manufacturer: record.Drop_down.value,
-          value: record.Number_0.value
-        }];
+        console.log('allData');
+        console.log(allData);
+        chart.data = JSON.parse(JSON.stringify(allData[year]));
 
-        console.log(record.Number.value);
-        console.log(allData[record.Number.value]);
+        categoryAxis.zoom({
+          start: 0,
+          end: 1 / chart.data.length
+        });
 
+      },
+      function (error) {
+        // Error
+        console.log(error);
       });
-
-      chart.data = JSON.parse(JSON.stringify(allData[year]));
-
-      categoryAxis.zoom({
-        start: 0,
-        end: 1 / chart.data.length
-      });
-
-    }, function (error) {
-      // Error
-      console.log(error);
-    });
 
     // Register the auto play function
-    series.events.on("inited", function () {
+    series.events.on('inited', function () {
       play();
     });
   });
