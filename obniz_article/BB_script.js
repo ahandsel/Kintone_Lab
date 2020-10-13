8594928abc6adc619a6d5695d3f09e3ed9f06cf2
Genji <ahandsel@gmail.com>
2020-10-13 16:54:27 +0900

    Added Obniz related comments

--- obniz_article/BB_script.js
+++ obniz_article/BB_script.js
@@ -1,9 +1,14 @@
 (() => {
   'use strict';
+
+  // Instantiate Obniz with Obniz ID & set callback function
   const obniz = new Obniz('XXXX-XXXX');
   kintone.events.on('app.record.index.show', event => {
     obniz.onconnect = async () => {
       let count = 0;
+
+      // Gets the input from the built-in black switch
+      // Switch's 4 states: "none", "push", "left", "right"
       obniz.switch.onchange = function (state) {
         obniz.display.clear();
         if (state === 'right') {
@@ -13,6 +18,7 @@
         } else if (state === 'push') {
           kintone.api();
         }
+        // Display the current count on Obniz
         obniz.display.print(count);
       };
     };
