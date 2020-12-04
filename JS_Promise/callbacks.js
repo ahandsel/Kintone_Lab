8c7be34fa10eefb39c5d5cbf44d7979bac6d0fb0
Genji <ahandsel@gmail.com>
2020-12-04 18:23:59 +0900

    Create callbacks.js

---
+++ JS_Promise/callbacks.js
@@ -0,0 +1,15 @@
+const posts = [
+  { tile: 'Post One', body: 'This is the first post'},
+  { tile: 'Post Two', body: 'This is the second post'},
+];
+
+const timeout = 1000;
+
+function getPosts() {
+  setTimeout(() => {
+    let output = '';
+    posts.forEach((post) => {
+      
+    });
+  }, timeout);
+}
