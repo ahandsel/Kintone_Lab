d523e25e90d531c47f6a6b4c869a16a2d08e3d16
Genji Fujimori <30670749+ahandsel@users.noreply.github.com>
2021-02-15 14:26:39 +0900

    Using imgur instead of GitHub as Presidents' photo provider

--- amChart_TimeLine/TimeLine_Test.js
+++ amChart_TimeLine/TimeLine_Test.js
@@ -3,6 +3,7 @@
 (function () {
   'use strict';
 
+  // Color HEX code for the political parties
   const partyColor = {
     'Democratic': '#2502fe',
     'Democratic-Republican': '#0e7003',
@@ -11,6 +12,9 @@
     'Unaffiliated': '#d5d5d5',
     'Whig': '#ebbd50'
   };
+
+  // Imgur's Image Hash
+  const imgCode = ['', 'gk1cP2u', '9FR7eva', 'aumfl2F', 'kx8BdCT', 'z6Yd9Lm', '4HLWKcB', 'mmTyKJ1', 'PCb7ly6', 'UgN4EA7', 'rVoiW4X', 'i3euA9w', 'kt92UVi', 'oo5aw5x', 'BZI7FTa', 'XLkIWAs', 'qB4GVSv', '5kAZ3NJ', 'drQysn9', 'UqvMVTH', 'Wte60Q6', '3DAK1C1', 'h6b7Vw5', 'ctHNmhA', '18zBca0', 'hb94eO2', '12hJdg0', 'Bl8oRVY', 'HQ1xV6H', 'pj4oAjR', 'QuzzGmm', 'zpE50Pa', '4zSuesA', 'mevGXAh', '5zq1fDg', 'EcUoEs2', 'S5BJfEO', 'bMW0Ltr', 'hutxiiz', 'hRBgyF1', 'gHP21eA', '3QtuHuW', 'XSwwVLh', 'wnbeSN1', 'Uw18RFA', '4APgxYd', 'S5wCZcd'];
 
   // Kintone event triggered after the record list page is displayed.
   kintone.events.on('app.record.index.show', function (event) {
@@ -44,7 +48,7 @@
         'start': records.start.value,
         'end': records.end.value,
         'textDisabled': false,
-        'icon': `https://github.com/ahandsel/US-Presidents/raw/main/img/President_${records.Number.value}.png`,
+        'icon': `https://i.imgur.com/${imgCode[records.Number.value]}.png`,
         'category': ''
       }
     });
