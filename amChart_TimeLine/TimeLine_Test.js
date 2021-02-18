bcc4de6d3d51dfb21c89548b33e8feb54aea07b7
Genji Fujimori <30670749+ahandsel@users.noreply.github.com>
2021-02-18 23:29:16 +0900

    Added tortoise

--- amChart_TimeLine/TimeLine_Test.js
+++ amChart_TimeLine/TimeLine_Test.js
@@ -1,4 +1,5 @@
 // amChart TimeLine x Kintone
+// Added Tortoise
 
 (function () {
   'use strict';
@@ -13,34 +14,16 @@
     'Whig': '#ebbd50'
   };
 
-  function getPhotoLink(URL) {
-    // const URL = 'https://en.wikipedia.org/wiki/Presidency_of_Thomas_Jefferson';
-    let wikiTitle = (URL).replace('https\:\/\/en\.wikipedia\.org\/wiki\/', '')
-    let wikiQuery = `https://en.wikipedia.org/w/api.php?action=query&prop=pageimages&titles=${wikiTitle}&pithumbsize=100&format=json`;
-
-    return new kintone.Promise(
-      function (resolve, reject) {
-        kintone.proxy(wikiQuery, 'GET', {}, {},
-          function (resp) {
-            let respJSON = JSON.parse(resp).query.pages;
-
-            let pageid = Object.keys(respJSON)[0];
-            var imgURL = respJSON[pageid].thumbnail.source;
-            console.log(`URL: ${URL}`);
-            console.log(`imgURL: ${imgURL}`);
-            resolve(imgURL);
-          },
-          function (err) {
-            console.log(err);
-            reject(err);
-          }
-        );
-      }
-    );
-  }
+  // Imgur's Image Hash
+  const imgCode = ['', 'gk1cP2u', '9FR7eva', 'aumfl2F', 'kx8BdCT', 'z6Yd9Lm', '4HLWKcB', 'mmTyKJ1', 'PCb7ly6', 'UgN4EA7', 'rVoiW4X', 'i3euA9w', 'kt92UVi', 'oo5aw5x', 'BZI7FTa', 'XLkIWAs', 'qB4GVSv', '5kAZ3NJ', 'drQysn9', 'UqvMVTH', 'Wte60Q6', '3DAK1C1', 'h6b7Vw5', 'ctHNmhA', '18zBca0', 'hb94eO2', '12hJdg0', 'Bl8oRVY', 'HQ1xV6H', 'pj4oAjR', 'QuzzGmm', 'zpE50Pa', '4zSuesA', 'mevGXAh', '5zq1fDg', 'EcUoEs2', 'S5BJfEO', 'bMW0Ltr', 'hutxiiz', 'hRBgyF1', 'gHP21eA', '3QtuHuW', 'XSwwVLh', 'wnbeSN1', 'Uw18RFA', '4APgxYd', 'S5wCZcd', 'btAPIEa'];
 
   // Kintone event triggered after the record list page is displayed.
   kintone.events.on('app.record.index.show', function (event) {
+
+    // Peak inside Kintone's data
+    console.log('event.records');
+    console.log(event.records);
+
     // Retrieve & configure the space element below the record list's header
     const spaceDiv = kintone.app.getHeaderSpaceElement();
     spaceDiv.style.height = '650px';
@@ -54,72 +37,60 @@
 
     // Create chart instance
     const chart = am4core.create(spaceDiv, am4plugins_timeline.SerpentineChart);
-    chart.autoMargins = true;
-    chart.paddingTop = '1px';
-    chart.paddingTop = '70px';
+    // Space between the chart & border
+    chart.paddingTop = 100;
+    // Number of straight lines of serpentine shape
     chart.levelCount = 5;
-    chart.maskBullets = false; // Allow bullets to "bleed" over the edge
+    // Allow bullets to 'bleed' over the edge
+    chart.maskBullets = false;
+    // Input & Output Date format
     chart.dateFormatter.inputDateFormat = 'yyyy-MM-dd';
     chart.dateFormatter.dateFormat = 'yyyy-MM-dd';
+    // Font size
     chart.fontSize = 12;
     chart.tooltipContainer.fontSize = 12;
-    chart.data = event.records.map((records, index) => {
-      getPhotoLink(records.Link.value).then(
-        function (resp) {
-          console.log(resp);
-          return {
-            'text': `${records.Text.value}\n${records.Text_0.value}`,
-            'color': partyColor[records.Drop_down.value],
-            'start': records.Date.value,
-            'end': records.Date_0.value,
-            'textDisabled': false,
-            // 'icon': a,
-            // 'icon': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/John_Vanderlyn_-_James_Madison_-_Google_Art_Project.jpg/85px-John_Vanderlyn_-_James_Madison_-_Google_Art_Project.jpg',
-            'category': ''
-          }
-        }
-      );
+
+    // Input Kintone data into the chart
+    chart.data = event.records.map((rec, index) => {
+      return {
+        // Text above the PinBullet; President's name
+        'text': `${rec.Text.value}\n${rec.Text_0.value}`,
+        // PinBullet's & time period's color; Party color
+        'color': partyColor[rec.Drop_down.value],
+        // Time period's start; Term's start
+        'start': rec.Date.value,
+        // Time period's end; Term's end
+        'end': rec.Date_0.value,
+        // Icon inside the PinBullet; President's icon
+        'icon': `https://i.imgur.com/${imgCode[rec.Record_number.value]}.png`,
+        // Timeline category; only 1 is needed
+        'category': rec.Drop_down_1.value
+      }
     });
 
     console.log('chart.data');
     console.log(chart.data);
 
+    // Create 1 timeline with all the US Presidents
     const categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
     categoryAxis.dataFields.category = 'category';
 
-    // Timeline axis
+    // Axis using date & time scale
     const dateAxis = chart.xAxes.push(new am4charts.DateAxis());
-    dateAxis.renderer.minGridDistance = 70;
-    // Specify the time unit
-    dateAxis.baseInterval = {
-      count: 2,
-      timeUnit: 'years'
-    };
-    dateAxis.renderer.tooltipLocation = 0;
+    // Gray, dashed lines for date axis
     dateAxis.renderer.line.strokeDasharray = '1,4';
     dateAxis.renderer.line.strokeOpacity = 1;
-    dateAxis.tooltip.background.fillOpacity = 0.2;
-    dateAxis.tooltip.background.cornerRadius = 5;
-    dateAxis.tooltip.label.fill = new am4core.InterfaceColorSet().getFor('alternativeBackground');
-    dateAxis.tooltip.label.paddingTop = 7;
-    dateAxis.endLocation = 0;
-    dateAxis.startLocation = -0.5;
-
-    // Timeline axis' label
-    const labelTemplate = dateAxis.renderer.labels.template;
-    labelTemplate.verticalCenter = 'middle';
-    labelTemplate.fillOpacity = 1;
-    labelTemplate.background.fill = new am4core.InterfaceColorSet().getFor('background');
-    labelTemplate.background.fillOpacity = 1;
-    labelTemplate.padding(7, 7, 7, 7);
+    // Place the label in the middle of the axis
+    dateAxis.renderer.labels.template.verticalCenter = 'middle';
 
     // Series containing the US Presidents and their terms
     const series = chart.series.push(new am4plugins_timeline.CurveColumnSeries());
-    series.columns.template.height = am4core.percent(15);
     series.dataFields.openDateX = 'start';
     series.dataFields.dateX = 'end';
     series.dataFields.categoryY = 'category';
     series.baseAxis = categoryAxis;
+    // Coloring of the Presidential terms
+    series.columns.template.height = am4core.percent(15);
     series.columns.template.propertyFields.fill = 'color';
     series.columns.template.propertyFields.stroke = 'color';
     series.columns.template.strokeOpacity = 0;
@@ -127,35 +98,45 @@
 
     // Create the PinBullet (Circles)
     const pinBullet = series.bullets.push(new am4plugins_bullets.PinBullet());
-    pinBullet.locationX = 1;
+    pinBullet.locationX = 1; //Place the PinBullet at their term's start
     pinBullet.propertyFields.stroke = 'color';
     pinBullet.background.propertyFields.fill = 'color';
     pinBullet.image = new am4core.Image();
     pinBullet.image.propertyFields.href = 'icon';
 
     // President's name over the icon
-    const textBullet = series.bullets.push(new am4charts.LabelBullet());
-    textBullet.label.propertyFields.text = 'text';
-    textBullet.disabled = true;
-    textBullet.propertyFields.disabled = 'textDisabled';
-    textBullet.label.strokeOpacity = 0;
-    textBullet.locationX = 1;
-    textBullet.dy = -80;
-    textBullet.label.textAlign = 'middle';
+    const labelBullet = series.bullets.push(new am4charts.LabelBullet());
+    labelBullet.label.propertyFields.text = 'text';
+    labelBullet.label.textAlign = 'middle';
+    labelBullet.locationX = 1; // Place the labelBullet at their term's start
+    labelBullet.dy = -80; // Raising text above the icon
 
     // Scrollbar used to focus the timeline
     chart.scrollbarX = new am4core.Scrollbar();
     chart.scrollbarX.align = 'center'
     chart.scrollbarX.width = am4core.percent(75);
-    chart.scrollbarX.opacity = 0.5;
     chart.scrollbarX.parent = chart.bottomAxesContainer;
 
     // Year appearing when hovering over the chart axis
     const cursor = new am4plugins_timeline.CurveCursor();
     chart.cursor = cursor;
+    dateAxis.tooltipDateFormat = 'yyyy-MMM';
     cursor.xAxis = dateAxis;
-    cursor.yAxis = categoryAxis;
-    cursor.lineY.disabled = true;
-    dateAxis.renderer.tooltipLocation2 = 0.5;
+    cursor.lineY.disabled = true; // Disable Y line highlight
+
+    // Optional - Enable export
+    chart.exporting.menu = new am4core.ExportMenu();
+    // Remove unneeded Scrollbar & tooltip
+    chart.scrollbarX.exportable = false;
+    dateAxis.tooltip.exportable = false;
+    // chart.exporting.menu.items = [{
+    //   'label': '...',
+    //   'menu': [
+    //     { 'type': 'png', 'label': 'PNG' },
+    //   ]
+    // }];
+    // var options = chart.exporting.getFormatOptions('png');
+    // options.scale = 5;
+    // chart.exporting.setFormatOptions('png', options);
   });
 })();
