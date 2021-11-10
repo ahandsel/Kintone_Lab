b1ab637f18bd1860f0c63d368978ba86ecd94984
ahandsel <genji-fujimori@cybozu.co.jp>
2021-11-11 06:19:19 +0900

    Still works!

---
+++ React_Search_Bar/React_CDN_Test/like_button.js
@@ -0,0 +1,31 @@
+(function () {
+  "use strict";
+
+  kintone.events.on("app.record.index.show", function (event) {
+
+    const e = React.createElement;
+
+    class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+      }
+  
+      render() {
+        if (this.state.liked) {
+          return "You liked this.";
+        }
+  
+        return e(
+          "button",
+          { onClick: () => this.setState({ liked: true }) },
+          "Like"
+        );
+      }
+    }
+  
+    const domContainer = document.querySelector("#like_button_container");
+    // ReactDOM.render(e(LikeButton), domContainer);
+    ReactDOM.render( /*#__PURE__*/React.createElement("h1", null, "Hello, world!"), document.querySelector("#like_button_container"));
+  });
+})();
