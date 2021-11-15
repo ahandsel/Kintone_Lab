e506efc781593e9b4c8d8254c4112b95b72936e3
ahandsel <ahandsel@gmail.com>
2021-11-16 07:18:22 +0900

    Not working - Error: Target Container is not a DOM Element

--- React_Search_Bar/React_CDN_Test/like_button.js
+++ React_Search_Bar/React_CDN_Test/like_button.js
@@ -1,21 +1,40 @@
+// Status: Does not work with Kintone
+// Error: Target Container is not a DOM Element
+
+// CDN URLs
+// https://unpkg.com/react@17/umd/react.development.js
+// https://unpkg.com/react-dom@17/umd/react-dom.development.js
+
 (function () {
   "use strict";
+
+  // function loadJS(src) {
+  //   document.write('<script type="text/javascript" src="' + src + '"></script>');
+  // }
+
+  // // Development versions of React
+  // loadJS('https://unpkg.com/react@17/umd/react.development.js');
+  // loadJS('https://unpkg.com/react-dom@17/umd/react-dom.development.js');
+
+  // Minified and optimized production versions of React
+  // loadJS('https://unpkg.com/react@17/umd/react.production.min.js');
+  // loadJS('https://unpkg.com/react-dom@17/umd/react-dom.production.min.js');
 
   kintone.events.on("app.record.index.show", function (event) {
 
     const e = React.createElement;
 
     class LikeButton extends React.Component {
-    constructor(props) {
+      constructor(props) {
         super(props);
         this.state = { liked: false };
       }
-  
+
       render() {
         if (this.state.liked) {
           return "You liked this.";
         }
-  
+
         return e(
           "button",
           { onClick: () => this.setState({ liked: true }) },
@@ -23,9 +42,9 @@
         );
       }
     }
-  
-    const domContainer = document.querySelector("#like_button_container");
+
+    // const domContainer = document.querySelector("#like_button_container");
     // ReactDOM.render(e(LikeButton), domContainer);
-    ReactDOM.render( /*#__PURE__*/React.createElement("h1", null, "Hello, world!"), document.querySelector("#like_button_container"));
+    ReactDOM.render(React.createElement("h1", null, "Hello, world!"), document.querySelector("#like_button_container"));
   });
 })();
