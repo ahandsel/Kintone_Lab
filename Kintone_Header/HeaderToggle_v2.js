14f8b7eb241957b4562225c27e5bad9411c5d868
Genji Fujimori <genji-fujimori@cybozu.co.jp>
2022-09-30 17:36:32 +0900

    HeaderToggle_v2.js is working & is a bit better

---
+++ Kintone_Header/HeaderToggle_v2.js
@@ -0,0 +1,50 @@
+// Add a button to show & hide the Kintone header
+(function () {
+  'use strict';
+  // Run a function when the record list page appears
+  kintone.events.on('app.record.index.show', function (event) {
+    // Prevent duplication of the button
+    if (document.getElementById('toggle_button') != null) {
+      return;
+    }
+    // Set a button
+    const toggleButton = document.createElement('input');
+    toggleButton.setAttribute('type', 'checkbox');
+    toggleButton.name = 'Hi'
+    toggleButton.id = 'toggle_button';
+    toggleButton.style.margin = '10px';
+    toggleButton.style.padding = '2px 2px';
+
+    const toggleLabel = document.createElement('label');
+    toggleLabel.htmlFor = 'toggle_button';
+    toggleLabel.appendChild(document.createTextNode('Header Toggle'));
+
+    const br = document.createElement('br');
+
+    // Button onclick function
+    toggleButton.onclick = function () {
+      headerToggle();
+    };
+
+    // Retrieve the header menu space element and set the button there
+    const container = kintone.app.getHeaderSpaceElement();
+    container.appendChild(toggleButton);
+    container.appendChild(toggleLabel);
+    container.appendChild(br);
+
+    // const record = event.record;
+    function headerToggle() {
+      let header = document.getElementsByClassName('gaia-argoui-app-index-toolbar')[0];
+      if (header.style.display === 'none') {
+        header.style.display = 'block';
+        console.log('Showing the header');
+        toggleButton.innerHTML = 'Show Header!';
+      } else {
+        header.style.display = 'none';
+        console.log('Hiding the header');
+        toggleButton.innerHTML = 'Hide Header!';
+      }
+    }
+  });
+
+})();
---
+++ Kintone_Header/Notes.md
@@ -0,0 +1,13 @@
+# Notes on Showing & Hiding the Kintone Header
+
+## Code Versions
+
+[HeaderToggle_v1.js](HeaderToggle_v1.js)
+* Crude, but it works 💪
+* Only a simple button to toggle the header
+
+[HeaderToggle_v2.js](HeaderToggle_v2.js)
+* Working, now a checkbox to toggle the header
+* Things to improve on:
+  * Converting a checkbox to a switch
+  * Move text to in front of the checkbox
