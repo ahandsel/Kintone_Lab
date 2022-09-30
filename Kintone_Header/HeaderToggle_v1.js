dc41df76aa0db5b1e3b2f43bf999300bdf166a1b
Genji Fujimori <genji-fujimori@cybozu.co.jp>
2022-09-30 23:43:33 +0900

    Update HeaderToggle_v1.js

--- Kintone_Header/HeaderToggle_v1.js
+++ Kintone_Header/HeaderToggle_v1.js
@@ -1,4 +1,5 @@
 // Add a button to show & hide the Kintone header
+// If this code breaks, verify if `gaia-argoui-app-index-toolbar` is still the correct class name for the header
 (function () {
   'use strict';
   // Run a function when the record list page appears
@@ -10,7 +11,13 @@
     // Set a button
     const toggleButton = document.createElement('button');
     toggleButton.id = 'toggle_button';
-    toggleButton.innerHTML = 'Hide Header!';
+    toggleButton.innerHTML = 'Hide Header';
+    toggleButton.style.margin = '10px';
+    toggleButton.style.padding = '10px 10px';
+    toggleButton.style.ariaLabel = 'Header Display Toggle';
+    toggleButton.style.height = '48px';
+    toggleButton.style.border = '1px solid #e3e7e8';
+    toggleButton.style.backgroundColor = '#f7f9fa';
 
     // Button onclick function
     toggleButton.onclick = function () {
@@ -25,11 +32,11 @@
       if (header.style.display === "none") {
         header.style.display = "block";
         console.log('Showing the header');
-        toggleButton.innerHTML = 'Show Header!';
+        toggleButton.innerHTML = 'Hide Header';
       } else {
         header.style.display = "none";
         console.log('Hiding the header');
-        toggleButton.innerHTML = 'Hide Header!';
+        toggleButton.innerHTML = 'Show Header';
       }
     }
   });
