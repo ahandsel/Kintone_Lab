029f45a41efded804d581f69ab8f37017e019618
Genji Fujimori <genji-fujimori@cybozu.co.jp>
2022-09-30 23:43:44 +0900

    Update HeaderToggle_v2.js

--- Kintone_Header/HeaderToggle_v2.js
+++ Kintone_Header/HeaderToggle_v2.js
@@ -4,23 +4,47 @@
   // Run a function when the record list page appears
   kintone.events.on('app.record.index.show', function (event) {
     // Prevent duplication of the button
-    if (document.getElementById('toggle_button') != null) {
-      return;
-    }
-    // Set a button
+    if (document.getElementById('toggle') !== null) { return; }
+
+    // Label for the switch
+    const toggleLabel = document.createElement('label');
+    toggleLabel.htmlFor = 'toggle';
+    // toggleLabel.appendChild(document.createTextNode('Header Toggle'));
+
+    // Set the switch
     const toggleButton = document.createElement('input');
     toggleButton.setAttribute('type', 'checkbox');
-    toggleButton.name = 'Hi'
-    toggleButton.id = 'toggle_button';
+    toggleButton.id = 'toggle';
     toggleButton.style.margin = '10px';
     toggleButton.style.padding = '2px 2px';
+    // toggleButton.style.display = 'none';
+    toggleButton.appendChild(document.createTextNode('Header Toggle'));
 
-    const toggleLabel = document.createElement('label');
-    toggleLabel.htmlFor = 'toggle_button';
-    toggleLabel.appendChild(document.createTextNode('Header Toggle'));
 
-    const br = document.createElement('br');
+    // Toggle Wrapper span
+    const toggleDiv = document.createElement('div');
+    toggleDiv.class = 'toggle-wrapper';
+    toggleDiv.style.position = 'relative';
+    toggleDiv.style.width = '120px';
+    toggleDiv.style.height = '60px';
+    toggleDiv.style.backgroundColor = '#eaeaea';
+    toggleDiv.style.borderRadius = '999px';
+    toggleDiv.style.margin = 'auto';
+    toggleDiv.style.cursor = 'pointer';
+    toggleDiv.style.pointerEvents = 'all';
 
+    // Toggle selector
+    const toggleSpan = document.createElement('span');
+    toggleSpan.class = 'selector';
+    toggleSpan.style.width = '40px';
+    toggleSpan.style.height = '40px';
+    toggleSpan.style.position = 'absolute';
+    toggleSpan.style.top = '50%';
+    toggleSpan.style.left = '10px';
+    toggleSpan.style.transform = 'translateY(-50%)';
+    toggleSpan.style.backgroundColor = '#3957ee';
+    toggleSpan.style.transition = 'left 0.25s ease';
+    toggleSpan.style.borderRadius = '50%';
     // Button onclick function
     toggleButton.onclick = function () {
       headerToggle();
@@ -28,9 +52,10 @@
 
     // Retrieve the header menu space element and set the button there
     const container = kintone.app.getHeaderSpaceElement();
-    container.appendChild(toggleButton);
     container.appendChild(toggleLabel);
-    container.appendChild(br);
+    toggleLabel.appendChild(toggleButton);
+    toggleLabel.appendChild(toggleDiv);
+    toggleDiv.appendChild(toggleSpan);
 
     // const record = event.record;
     function headerToggle() {
@@ -43,6 +68,10 @@
         header.style.display = 'none';
         console.log('Hiding the header');
         toggleButton.innerHTML = 'Hide Header!';
+        toggleDiv.style.left = 'calc(100% - 50px)';
+        toggleDiv.style.backgroundColor = '#ee5f39';
+        toggleSpan.style.left = 'calc(100% - 50px)';
+        toggleSpan.style.backgroundColor = '#ee5f39';
       }
     }
   });
